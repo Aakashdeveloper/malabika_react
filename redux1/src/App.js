3147036cfe28193e5119118ae006592638e880ad
aakashDeveloper <ahanda205@gmail.com>
2018-11-23 00:07:18 +0000

    a

--- redux1/src/App.js
+++ redux1/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from './actions';
-
+import MoviesList from './containers/movies_list';
 
 class App extends Component {
 
-  componentWillMount(){
-    this.props.moviesList()
-  }
 
   render() {
     return (
-      <h1> Hellow to redux</h1>
+      <div>
+        <MoviesList></MoviesList>
+      </div>
     );
   }
 }
