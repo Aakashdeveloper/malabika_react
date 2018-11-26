189ed7fb7ad5789e56f4854c4e70eb9834695e89
aakashDeveloper <ahanda205@gmail.com>
2018-11-26 04:35:34 +0000

    a

---
+++ redux2/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello to redux
+      </div>
+    );
+  }
+}
+
+export default App;
